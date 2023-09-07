12e54fc0965f2a6b6ce4ac03de110ba34411dd46
= <=>
2023-09-07 15:01:16 -0300

    estructura

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,11 +1,6 @@
-
-
 function cargar() {
     //alert("cargad");
     //document.getElementById("cont-footer").innerHTML=" ";
     
     
-}
-function verificar() {
-    alert("BIENVENIDO -variable:name-");
 }
